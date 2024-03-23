69ac61769c33f3c0b7d7b4a7cf4483a37548c81e
mehmedsherif <mehmedali18@abv.bg>
2024-03-23 20:21:40 +0200

    Upload

---
+++ C# OOP/UnitTesting-Exercise/DatabaseExtended.Tests/PersonTests.cs
@@ -0,0 +1,22 @@
+﻿using ExtendedDatabase;
+using NUnit.Framework;
+using System;
+
+namespace DatabaseExtended.Tests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        private const string ValidUserName = "pesho123";
+        private const long ValidId = 100001;
+
+        [Test]
+        public void Ctor_WithValidParameters_CreateNewInstance()
+        {
+            Person person = new Person(ValidId, ValidUserName);
+            Assert.That(person, Is.Not.Null);
+            Assert.That(person.Id, Is.EqualTo(ValidId));
+            Assert.That(person.UserName, Is.EqualTo(ValidUserName));
+        }
+    }
+}
